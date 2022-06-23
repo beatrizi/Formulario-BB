6d177bba2ced663515b5ef991d33f096f17b8a09
Beatrizi <beatrici25@gmail.com>
2022-06-23 19:32:36 +0100

    comentario, definicion de cliente y servidor usando js

--- recursos/boton/JS-en-el-servidor.js
+++ recursos/boton/JS-en-el-servidor.js
@@ -19,4 +19,13 @@ console.log('the file helloWord.txt ha sido creado')
 let howManyPizza = require('how-many-pizza')
 console.log('necesitamos %s pizas para 8 personas',howManyPizza(8))
 
-/**/
+/*
+Js en el cliente vs Js en el servidor
+cliente== se ejecuta en un navegador web mediante tags de <script></script>. 
+        tiene acceso a las funciones y objetos en la pagina web.
+servidor== se ejecuta mediante Node.js y tiene acceso a paquetes que permiten interactuar con el sist operativo.
+        usualmente se usa para construir servicios web.
+El mismo JS en todos los lados, si bien las funcioalidades disponibles son distintas en el navegador y en NodeJS, la logica que implementes puede ser la misma. La comunidad nos ofrece librerias para ambos.
+
+
+*/
