a43ddcf7a91ba0632b5a374e948bce88c6ba9f5b
Beatrizi <beatrici25@gmail.com>
2022-06-24 12:14:46 +0100

    added setimeout, pero no hace nada

--- recursos/boton/index.js
+++ recursos/boton/index.js
@@ -1,12 +1,17 @@
 //creamos la variable para llamar a un elemento html que tiene el id se llama submit
 let submitBtn = document.getElementById('submit');
+submitBtn.style='background-color: #9858a8';
 
 
 
 submitBtn.addEventListener('click', cosas)
 
 function cosas(params) {
-    //agregamos color al boton verde
-submitBtn.style='background-color: green';
-alert('hello js')
+    //cambiamos color al boton verde con el click
+submitBtn.style='background-color: #8fc98b';
+    // despues de 2 segundo salta la aterta
+    // alert('hello js')
+setTimeout(alert('hello js'), 5000);
+//The setTimeout() method calls a function after a number of milliseconds.
+//1 second = 1000 milliseconds.
 }
